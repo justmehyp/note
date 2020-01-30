00abdeb9dfd9ab5c297f017b8daa08cbbf5ef0f7
hyp <hyp>
2020-01-30 17:20:09 +0800

    add

--- jvm/src/jvm/bytecode/decompiler/attributeinfo/CodeAttributeInfo.java
+++ jvm/src/jvm/bytecode/decompiler/attributeinfo/CodeAttributeInfo.java
@@ -64,4 +64,20 @@ public class CodeAttributeInfo extends AttributeInfo {
         }
         return attributes;
     }
+
+    public static class ExceptionTable {
+        public short startPC;
+        public short endPC;
+        public short handlerPC;
+        public short catchType;
+
+        public static ExceptionTable build(DataInputStreamReader reader) throws IOException {
+            ExceptionTable exceptionTable = new ExceptionTable();
+            exceptionTable.startPC = reader.u2();
+            exceptionTable.endPC = reader.u2();
+            exceptionTable.handlerPC = reader.u2();
+            exceptionTable.catchType = reader.u2();
+            return exceptionTable;
+        }
+    }
 }
