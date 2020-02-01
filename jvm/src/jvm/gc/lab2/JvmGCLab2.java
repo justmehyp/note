4f0e4496b3e49f0478d0d8a323b061ff4e6c0ddc
hyp <hyp>
2020-02-01 14:58:47 +0800

    add

---
+++ jvm/src/jvm/gc/lab2/JvmGCLab2.java
@@ -0,0 +1,14 @@
+package jvm.gc.lab2;
+
+public class JvmGCLab2 {
+
+    public static void main(String[] args) {
+        int m = 1024 * 1024;
+
+//        byte[] bytes = new byte[1 * m];
+        byte[] bytes = new byte[2 * m];
+
+        System.out.println("hello world");
+
+    }
+}
