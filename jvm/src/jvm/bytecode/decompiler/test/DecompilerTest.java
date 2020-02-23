d1041c5822e4b727557be32b781eb8fbc0d44a04
hyp <hyp>
2020-02-23 18:10:25 +0800

    add

--- jvm/src/jvm/bytecode/decompiler/test/DecompilerTest.java
+++ jvm/src/jvm/bytecode/decompiler/test/DecompilerTest.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class DecompilerTest {
     public static void main(String[] args) throws IOException {
-//        String path = "/Users/hyp/code/note/jvm/target/classes/jvm/bytecode/lab1/JvmBytecoeLab1.class";
-        String path = "/tmp/rt/java/lang/Object.class";
+        String path = "/Users/hyp/code/note/jvm/target/classes/jvm/bytecode/lab1/JvmBytecoeLab1.class";
+//        String path = "/tmp/rt/java/lang/Object.class";
         Decompiler decompiler = new Decompiler(path);
         decompiler.decompile(System.out);
     }
